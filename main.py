13d4dc158b0d23d26e1abf2f687b716d9fa7db13
Paulo Sampaio <paulovictorsampaio@hotmail.com>
2023-01-17 21:44:02 -0300

    chrome

--- main.py
+++ main.py
@@ -4,13 +4,21 @@ from webdriver_manager.firefox import GeckoDriverManager
 from webdriver_manager.chrome import ChromeDriverManager
 import scanner
 
-options = webdriver.FirefoxOptions()
-options.add_argument('--incognito')
-options.add_argument("disable-infobars")
+# options = webdriver.FirefoxOptions()
+options = webdriver.ChromeOptions()
+# options.binary_location = './geckodriver'
+# options.add_argument('--incognito')
+# options.add_argument("disable-infobars")
 options.add_argument('--headless') #Oculto
-options.add_argument("--disable-extensions")
+# options.add_argument("--disable-extensions")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 
-driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()), options=options)
+prefs = {"profile.managed_default_content_settings.images":2}
+options.headless = True
+options.add_experimental_option("prefs", prefs)
+
+# driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()), options=options)
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
 scanner.run(driver)
